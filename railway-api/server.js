b07a7e0e165a44711695e6bc6cc291ec9bcd7460
aidargersemov-prog <aidargersemov@gmail.com>
2026-08-23 23:38:55 +0300

    Add files via upload

--- railway-api/server.js
+++ railway-api/server.js
@@ -22,7 +22,7 @@ import {
 } from "./case-loot.js";
 
 const PORT = Number(process.env.PORT || 3000);
-const API_BUILD_ID = "railway-api-2026-08-23-promzona-camera-v83";
+const API_BUILD_ID = "railway-api-2026-08-23-promzona-legacy-materials-v84";
 const CREATE_CODE = process.env.CREATE_CODE || "";
 const DEFAULT_KEY = process.env.DEFAULT_KEY || "contra-revive-key";
 const DATA_PATH = process.env.DATA_PATH || path.join(process.cwd(), "data", "accounts.json");
@@ -43,7 +43,7 @@ const ASSET_BUNDLE_NAMES = new Set([
 const REMOTE_ASSET_BUNDLE_URLS = new Map([
   [
     "promzona.unity3d",
-    "https://media.githubusercontent.com/media/aidargersemov-prog/contra-city-api/b4a905f90daff036940178bfa8c8a326a2c14a69/railway-api/assetbundles/promzona.unity3d"
+    "https://media.githubusercontent.com/media/aidargersemov-prog/contra-city-api/1b1302577e9955a5a298d4f71b1555e3fbc65dee/railway-api/assetbundles/promzona.unity3d"
   ]
 ]);
 const MIGRATIONS_DIR = path.join(API_DIR, "migrations");
